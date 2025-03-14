3a17f14224112c39b75a02c0169c6ab768fd6f83
bhargavi-python <bavanam.bhargavi@gmail.com>
2025-03-14 16:45:25 +0530

    Update search.py

--- apple_project/lib/search.py
+++ apple_project/lib/search.py
@@ -14,10 +14,10 @@ class AppleWebsite:
     def navigate(self):
         self.page.goto("https://www.google.com")
 
-    def clickonwebsite(self):
-        self.page.wait_for_timeout(4000)
-        self.page.locator(SearchPage.search_loc).click()
-        self.page.locator(SearchPage.search_loc).fill("Apple")
-        self.page.locator(SearchPage.search_loc).press("Enter")
-        self.page.locator(SearchPage.locate_apple).click()
-(4000)
+#     def clickonwebsite(self):
+#         self.page.wait_for_timeout(4000)
+#         self.page.locator(SearchPage.search_loc).click()
+#         self.page.locator(SearchPage.search_loc).fill("Apple")
+#         self.page.locator(SearchPage.search_loc).press("Enter")
+#         self.page.locator(SearchPage.locate_apple).click()
+# (4000)
